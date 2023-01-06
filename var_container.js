f8c90bbf89135e25cff8dfcc0ac259ad05367253
Pon <75355001+Pon5092@users.noreply.github.com>
2023-01-06 22:01:06 +0900

    modify var_container blockDataを更新できるようになった
    関数update_blockdata()でblockDataを更新する

--- var_container.js
+++ var_container.js
@@ -1,4 +1,8 @@
 let isAbleInput = true;
+let justUpdate = false;
+const blockData = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const blockDataHight = 3;
+const blockDataWidth = 3;
 
 function get_is_able_input() {
     return isAbleInput;
@@ -6,9 +10,23 @@ function get_is_able_input() {
 
 function set_is_able_input(nowAbleInput) {
     if(typeof nowAbleInput !== "boolean"){
-        console.log("set_able_inputに代入された方が違います")
+        console.log("set_able_inputに代入された型が違います")
         return;
     }
     isAbleInput = nowAbleInput
+    return;
+}
+
+function update_blockdata(newBlockData){
+    if(true /*is_good_blockData(newBlockData)*/){
+        blockData.splice(0,blockDataHight)
+        console.log("neBlockData = " + newBlockData)
+        newBlockData.forEach((line) => {
+            console.log("line = " + line)
+            blockData.push(line)
+          })
+    } else{
+        console.log("update_blockdataに代入された値が正しくありません")
+    }
     return;
 }
