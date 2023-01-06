e5475e54782a4f37881a6deb971c0b7218b1a3cd
Pon <75355001+Pon5092@users.noreply.github.com>
2023-01-06 22:15:39 +0900

    modify var_container.js newBlockDataの大きさが正しいか判定できるようにした
    newBlockData配列が適切な大きさかか確認する処理を追加
    それがオブジェクトであること，二次元配列の高さ，幅を確認し間違っていたら更新を行わない

--- var_container.js
+++ var_container.js
@@ -17,8 +17,27 @@ function set_is_able_input(nowAbleInput) {
     return;
 }
 
+function is_good_blockData(newBlockData){
+    let is_good_data = true
+    if(typeof newBlockData !== "object"){
+        console.log("is_good_blockDataに代入された型が違います")
+        is_good_data = false
+    }
+    if(newBlockData.length !== blockDataHight){
+        console.log("is_good_blockDataに代入された高さが違います")
+        is_good_data = false
+    }
+    for (let i = 0; i < newBlockData.length; i++) {
+        if(newBlockData[i].length !== blockDataWidth){
+            console.log("is_good_blockDataに代入された" + i + "番目の幅が違います")
+            is_good_data = false
+        } 
+    }
+    return is_good_data
+}
+
 function update_blockdata(newBlockData){
-    if(true /*is_good_blockData(newBlockData)*/){
+    if(is_good_blockData(newBlockData)){
         blockData.splice(0,blockDataHight)
         console.log("neBlockData = " + newBlockData)
         newBlockData.forEach((line) => {
