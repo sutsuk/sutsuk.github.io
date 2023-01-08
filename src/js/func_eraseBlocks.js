8b291c30a56e36e042be0e9a7f7fff3aa7f220e9
sutsuk <107033259+sutsuk@users.noreply.github.com>
2023-01-08 13:53:38 +0900

    update func_eraseBlocks.js 削除可能行の探索を行う関数を作成
    search_erasableLine() で，削除可能な行のうち，インデックスの最も小さい行を探索する．
    erase_line(lineNum) で，指定行の各ブロックに対する連鎖消去を行う予定．現時点ではログ出力のみ行う．
    gameover() で，ゲームオーバー処理を行う予定．現時点ではログ出力のみ行う．

--- src/js/func_eraseBlocks.js
+++ src/js/func_eraseBlocks.js
@@ -2,3 +2,33 @@ const scoresPerBlock = 1;
 const scoreComboBonus = 0.1;
 const eraseThreshold = 4;
 const gameoverThreshold = blockDataHeight - 1;
+let scoreTotal = 0;
+
+function search_erasableLine() {
+  let j; let erasedLines = 0;
+  let currentBlockData = get_blockData();
+  for(let i = 0; i < blockDataHeight; i++) {
+    for(j = 0; j < blockDataWidth; j++)
+      if(currentBlockData[i][j] == 0) break;
+    if(j == blockDataWidth) {
+      erase_line(i);
+      break;
+    }
+  }
+  for(let i = gameoverThreshold; i < blockDataHeight; i++) {
+    for(let j = 0; j < blockDataWidth; j++) {
+      if(currentBlockData[i][j] > 0) {
+        gameover();
+        return;
+      }
+    }
+  }
+}
+
+function erase_line(lineNum) {
+  console.log("ライン" + lineNum + "を削除");
+}
+
+function gameover() {
+  console.log("ゲームオーバー");
+}
